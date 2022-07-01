3379bf132fdde5047c59a2b66f9e4e3e14bad462
CHire <paperbags@naver.com>
2022-07-01 16:13:44 +0900

    JS: Home contact Btn

--- main.js
+++ main.js
@@ -16,11 +16,21 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
+  console.log(event.target.dataset.link);
   if (link == null) {
     return;
   }
-
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-
+  
+ scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior:'smooth' });
+}
