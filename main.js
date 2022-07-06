26d1655f329916bcb3ede3b82d041cab709f27f9
CHire <paperbags@naver.com>
2022-07-07 00:36:56 +0900

    JS:remove active from nav

--- main.js
+++ main.js
@@ -58,15 +58,25 @@ arrowBtn.addEventListener('click', () => {
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
+
+
 workBtnContainer.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter ==null) {
     return;
   }
+
+  // Remove selection from the previous item and select the new one
+
+  const active = document.querySelector('.category__btn.selected');
+
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
+
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
     projects.forEach((project) => {
-      console.log(project.dataset.type);
       if(filter === '*' || filter === project.dataset.type) {
         project.classList.remove('invisible');
       } else {
@@ -75,6 +85,7 @@ workBtnContainer.addEventListener('click', (e) => {
     });
     projectContainer.classList.remove('anim-out');
   }, 300);
+
 });
 
 
